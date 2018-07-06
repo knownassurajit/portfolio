27140fa8b31d6f3b8996e6f000b9120a38012cdb
Surajit Das <isurajit123@gmail.com>
2018-07-06 23:44:44 +0530

    Lots of details modified in HTML and JS files.

--- js/google_map.js
+++ js/google_map.js
@@ -5,7 +5,7 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(25.0068539, 88.1473553);
     // 39.399872
     // -8.224454
     
